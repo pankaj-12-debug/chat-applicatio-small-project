af9e681346d6f582c8f2e6712b60727465267c57
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-10-21 00:55:47 +0530

    complete-task

--- demo.js
+++ demo.js
@@ -1,26 +1,34 @@
 const express= require('express');
+const fs=require('fs');
 const bodyParser=require('body-parser')
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
-const data=[];
+//const data=[];
 app.get('/login',(req,res,next)=>{
    // console.log('hi');
-    res.send('<form action="/detail"  onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" method="POST"><input  id="message" type="text" name="message" placeholder="message"> <input id="username" type="text" name="username"><button type="submit">login</button></form>');
+   fs.readFile('username.txt',(err,data)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    res.send(`${data}<form action="/detail"  onsubmit="localStorage.setItem('username', document.getElementById('username').value)" method="POST"><input  id="message" type="text" name="message" placeholder="message"> <input id="username" type="text" name="username"><button type="submit">login</button></form>`);
+   })
+  //  res.send(`${data}<form action="/detail"  onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" method="POST"><input  id="message" type="text" name="message" placeholder="message"> <input id="username" type="text" name="username"><button type="submit">login</button></form>`);
 })
 app.post('/detail',(req,res,next)=>{
- //app.push(`{${req.body.username}:${req.body.message}}`)
-    //console.log(req.body);
-  //  console.log(`{${req.body.username}:${req.body.message}}`);
-    res.redirect('/');
+ //data.push(`{${req.body.username}:${req.body.message}}`)
+    console.log(req.body);
+   console.log(`{${req.body.username}:${req.body.message}}`);
+   console.log('hi');
+   fs.writeFile("username.txt",`${req.body.username}:${req.body.message}`,{flag: 'a'},(err)=>{
+    console.log(err);
+    res.redirect('/login');
+   });
+   // res.redirect('/login');
 })
 app.get('/',(req,res,next)=>{
   //  console.log('chalo');
-  res.send('<form action="/"  onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" method="POST"><input  id="message" type="text" name="message" placeholder="message"> <input id="username" type="text" name="username"><button type="submit">login</button></form>');
+  res.send('<h1>hello</h1>');
 })
-app.post('/',(req,res,next)=>{
-    data.push(`{${req.body.username}:${req.body.message}}`);
-    console.log(req.body);
-   console.log(`{${req.body.username}:${req.body.message}}`);
-   res.redirect(`/`)
-})
+
 app.listen(4000);
